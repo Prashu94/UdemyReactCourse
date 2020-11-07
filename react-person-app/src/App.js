77811244f2ca6aa3bee9155257d89f265bfffc1e
Prashant <prashantbhat94@gmail.com>
2020-11-07 17:34:47 +0530

    New Data Commit

--- react-person-app/src/App.js
+++ react-person-app/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Person App</h1>
+      <Person name = "Prashant" age = {26}/>
+      <Person name = "Deval" age = {26}/>
+      <Person name = "Swapnil" age = {27}/>
     </div>
   );
 }
